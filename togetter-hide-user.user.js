29a2fc1e5fa8140941be968e14855dc5728472b6
recyclebin5385 <recyclebin5385@yahoo.co.jp>
2016-04-10 12:01:09 +0900

    説明文を修正する ライセンスを付与する

--- togetter-hide-user.user.js
+++ togetter-hide-user.user.js
@@ -1,20 +1,53 @@
 // ==UserScript==
 // @name        togetter-hide-user
-// @namespace   togetter-hide-user
+// @namespace   recyclebin5385
 // @description togetterの特定ユーザを見えなくする
 // @include     http://togetter.com/*
 // @version     1
 // @grant       none
 // ==/UserScript==
 
-/*
- * togetterのまとめのうち、特定のユーザが作成したものをまとめて非表示にします。  
- *
- * 非表示にするには、プロフィール画像アイコンをダブルクリックします。
- * 削除済みのまとめをダブルクリックすると再表示します。  
- *
- * 注意: デフォルトの卵アイコンには効果がありません。  
- */
+// Copyright (c) 2016, recyclebin5385
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// * Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice, 
+//   this list of conditions and the following disclaimer in the documentation 
+//   and/or other materials provided with the distribution.
+// * Neither the name of the <organization> nor the names of its contributors 
+//   may be used to endorse or promote products derived from this software 
+//   without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+//
+// 説明
+// ----
+//
+// togetterのまとめのうち、特定のユーザが作成したものをまとめて非表示にします。
+//
+// 非表示にするには、プロフィール画像アイコンをダブルクリックします。
+// 削除済みのまとめをダブルクリックすると再表示します。
+// 注意: デフォルトの卵アイコンには効果がありません。
+//
+//
+// 連絡先
+// ------
+// recyclebin5385[at]yahoo.co.jp ([at]を@に置換してください)
+//
+
 
 var urlToIdPattern = /\/profile_images\/([^\/]+)/;
 
@@ -112,6 +145,7 @@ function hideUsers() {
                 parentLi.after("<li class='clearfix dummy'></li>");
                 dummyParentLi = parentLi.next('.dummy');
 
+                dummyParentLi.append($(this).clone(true));
                 dummyParentLi.attr('title', parentLi.find('h3').text());
                 dummyParentLi.append("[削除済]");
                 dummyParentLi.dblclick(function() {
