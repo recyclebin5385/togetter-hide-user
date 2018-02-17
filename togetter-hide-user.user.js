ca9939a7c171af9707651e5601536f9d8a3a6b34
recyclebin5385 <recyclebin5385@yahoo.co.jp>
2018-02-17 18:02:12 +0900

    TampermonkeyでもGreasemonkeyでも動作するようにする

--- togetter-hide-user.user.js
+++ togetter-hide-user.user.js
@@ -5,7 +5,7 @@
 // @include     http://togetter.com/*
 // @include     https://togetter.com/*
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
-// @version     6
+// @version     7
 // @grant       none
 // ==/UserScript==
 
@@ -50,140 +50,144 @@
 // recyclebin5385[at]yahoo.co.jp ([at]を@に置換してください)
 //
 
+(function() {
+    jQuery.noConflict();
+    var $ = jQuery;
 
-var urlToIdPattern = /\/(?:default_)?profile_images\/([^\/]+)/;
+    var urlToIdPattern = /\/(?:default_)?profile_images\/([^\/]+)/;
 
-function urlToId(url) {
-    if(url.match(urlToIdPattern)) {
-        return RegExp.$1;
-    } else {
-        return null;
-    }
-}
-
-function getCookieMap() {
-    var ret = new Array();
-
-    var allCookies = document.cookie;
-    if( allCookies != '' ) {
-        var cookies = allCookies.split('; ');
-        for( var i = 0; i < cookies.length; i++ ) {
-            var cookie = cookies[i].split('=');
-
-            // クッキーの名前をキーとして 配列に追加する
-            ret[cookie[0]] = decodeURIComponent(cookie[1]);
+    function urlToId(url) {
+        if(url.match(urlToIdPattern)) {
+            return RegExp.$1;
+        } else {
+            return null;
         }
     }
 
-    return ret;
-}
+    function getCookieMap() {
+        var ret = new Array();
 
-function getHiddenUserIds() {
-    var cookieMap = getCookieMap();
-    var joinedHiddenUserIds = cookieMap['hiddenUserIds'];
-    if (joinedHiddenUserIds != null && joinedHiddenUserIds != '') {
-        return joinedHiddenUserIds.split(' ');
-    } else {
-        return new Array();
-    }
-}
+        var allCookies = document.cookie;
+        if( allCookies != '' ) {
+            var cookies = allCookies.split('; ');
+            for( var i = 0; i < cookies.length; i++ ) {
+                var cookie = cookies[i].split('=');
 
-function setHiddenUserIds(ids) {
-    var now = new Date();
-    var maxAgeDay = 30;
-    now.setTime(now.getTime() + maxAgeDay * 24 * 60 * 60 * 1000);
-    var expires = now.toGMTString();
-    var cookie = 'hiddenUserIds=' + encodeURIComponent(ids.join(' ')) + ";expires=" + expires;
+                // クッキーの名前をキーとして 配列に追加する
+                ret[cookie[0]] = decodeURIComponent(cookie[1]);
+            }
+        }
 
-    if (cookie.length > 4096) {
-        return false;
+        return ret;
     }
 
-    document.cookie = cookie;
-    hideUsers();
-    return true;
-}
-
-function addHiddenUserId(id) {
-    var ids = getHiddenUserIds();
-    if ($.inArray(id, ids) == -1) {
-        ids.push(id);
+    function getHiddenUserIds() {
+        var cookieMap = getCookieMap();
+        var joinedHiddenUserIds = cookieMap['hiddenUserIds'];
+        if (joinedHiddenUserIds != null && joinedHiddenUserIds != '') {
+            return joinedHiddenUserIds.split(' ');
+        } else {
+            return new Array();
+        }
     }
 
-    if (!setHiddenUserIds(ids)) {
-        var deleted = 0;
-        while (ids.length > 0) {
-            ids.shift();
-            deleted++;
-            if (setHiddenUserIds(ids)) {
-                alert("容量オーバーのため古いIDを" + deleted + "件削除しました。");
+    function setHiddenUserIds(ids) {
+        var now = new Date();
+        var maxAgeDay = 30;
+        now.setTime(now.getTime() + maxAgeDay * 24 * 60 * 60 * 1000);
+        var expires = now.toGMTString();
+        var cookie = 'hiddenUserIds=' + encodeURIComponent(ids.join(' ')) + ";expires=" + expires;
+
+        if (cookie.length > 4096) {
+            return false;
+        }
+
+        document.cookie = cookie;
+        hideUsers();
+        return true;
+    }
+
+    function addHiddenUserId(id) {
+        var ids = getHiddenUserIds();
+        if ($.inArray(id, ids) == -1) {
+            ids.push(id);
+        }
+
+        if (!setHiddenUserIds(ids)) {
+            var deleted = 0;
+            while (ids.length > 0) {
+                ids.shift();
+                deleted++;
+                if (setHiddenUserIds(ids)) {
+                    alert("容量オーバーのため古いIDを" + deleted + "件削除しました。");
+                    return;
+                }
+            }
+        }
+    }
+
+    function removeHiddenUserId(id) {
+        var ids = getHiddenUserIds();
+        var newIds = [];
+        for (var i = 0; i < ids.length; i++) {
+            if (id != ids[i]) {
+                newIds.push(ids[i]);
+            }
+        }
+        setHiddenUserIds(newIds);
+    }
+
+    function hideUsers() {
+        var hiddenUserIds = getHiddenUserIds();
+        $('.topics_box .icon_24').each(function() {
+            var id = urlToId($(this).attr('data-lazy-src'));
+            var parentLi = $(this).parents('li').filter(':not(.dummy)');
+            var dummyParentLi = $(this).parents('li').next('li.dummy');
+
+            if ($.inArray(id, hiddenUserIds) != -1) {
+                parentLi.hide();
+                if (dummyParentLi.length == 0) {
+                    parentLi.after("<li class='clearfix dummy'></li>");
+                    dummyParentLi = parentLi.next('.dummy');
+
+                    // 削除済のまとめに対してユーザのアイコンを出す場合は以下をアンコメントする
+                    // dummyParentLi.append($(this).clone(true).unbind('dblclick'));
+                    dummyParentLi.attr('title', parentLi.find('h3').text());
+                    dummyParentLi.append("[削除済]");
+                    dummyParentLi.dblclick(function() {
+                        if (confirm("このユーザを見えるようにしますか？")) {
+                            removeHiddenUserId(id);
+                        }
+                    });
+                };
+
+                dummyParentLi.show();
+            } else {
+                parentLi.show();
+                dummyParentLi.hide();
+            }
+        });
+    }
+
+    $(function() {
+        $('.topics_box .icon_24').dblclick(function() {
+            var id = urlToId($(this).attr('data-lazy-src'));
+            if (id == null || id == '') {
                 return;
             }
-        }
-    }
-}
 
-function removeHiddenUserId(id) {
-    var ids = getHiddenUserIds();
-    var newIds = [];
-    for (var i = 0; i < ids.length; i++) {
-        if (id != ids[i]) {
-            newIds.push(ids[i]);
-        }
-    }
-    setHiddenUserIds(newIds);
-}
-
-function hideUsers() {
-    var hiddenUserIds = getHiddenUserIds();
-    $('.topics_box .icon_24').each(function() {
-        var id = urlToId($(this).attr('data-lazy-src'));
-        var parentLi = $(this).parents('li').filter(':not(.dummy)');
-        var dummyParentLi = $(this).parents('li').next('li.dummy');
-
-        if ($.inArray(id, hiddenUserIds) != -1) {
-            parentLi.hide();
-            if (dummyParentLi.length == 0) {
-                parentLi.after("<li class='clearfix dummy'></li>");
-                dummyParentLi = parentLi.next('.dummy');
-
-                // 削除済のまとめに対してユーザのアイコンを出す場合は以下をアンコメントする
-                // dummyParentLi.append($(this).clone(true).unbind('dblclick'));
-                dummyParentLi.attr('title', parentLi.find('h3').text());
-                dummyParentLi.append("[削除済]");
-                dummyParentLi.dblclick(function() {
-                    if (confirm("このユーザを見えるようにしますか？")) {
-                        removeHiddenUserId(id);
-                    }
-                });
+            var hiddenUserIds = getHiddenUserIds();
+            if ($.inArray(id, hiddenUserIds) == -1) {
+                if (confirm("このユーザを見えなくしますか？")) {
+                    addHiddenUserId(id);
+                }
+            } else {
+                if (confirm("このユーザを見えるようにしますか？")) {
+                    removeHiddenUserId(id);
+                }
             }
+        });
 
-            dummyParentLi.show();
-        } else {
-            parentLi.show();
-            dummyParentLi.hide();
-        }
+        hideUsers();
     });
-}
-
-$(function() {
-    $('.topics_box .icon_24').dblclick(function() {
-        var id = urlToId($(this).attr('data-lazy-src'));
-        if (id == null || id == '') {
-            return;
-        }
-
-        var hiddenUserIds = getHiddenUserIds();
-        if ($.inArray(id, hiddenUserIds) == -1) {
-            if (confirm("このユーザを見えなくしますか？")) {
-                addHiddenUserId(id);
-            }
-        } else {
-             if (confirm("このユーザを見えるようにしますか？")) {
-                removeHiddenUserId(id);
-            }
-        }
-    });
-
-    hideUsers();
-});
+})();
